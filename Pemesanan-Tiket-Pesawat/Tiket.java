2c06dc251c693e4c7cb6cbd49cd62b95eab54e27
Vanguetta <auliamuzhaffar@gmail.com>
2022-11-07 13:08:46 +0700

    membuat javadoc class Tiket

--- Pemesanan-Tiket-Pesawat/Tiket.java
+++ Pemesanan-Tiket-Pesawat/Tiket.java
@@ -2,7 +2,7 @@
 /**
  * Abstract class Tiket - write a description of the class here
  *
- * @author adit
+ * @author aditP
  * @version (version number or date here)
  */
 public abstract class Tiket {
@@ -10,34 +10,57 @@ public abstract class Tiket {
     private int jumlahTiket;
 
     /**
-     * An example of a method - replace this comment with your own
+     * Method contructor
      *
-     * @param y a sample parameter for a method
-     * @return the sum of x and y
      */
     public Tiket() {
 
     }
 
+    /**method ini untuk mendapatkan nilai harga tiket
+     * @param hargaTiket
+     */
     public Tiket(Double hargaTiket) {
         this.harga_tiket = hargaTiket;
     }
 
+    /** 
+     * Method untuk mendapatkan/mengembalikan harga tiket
+     * @return harga_tiket
+     */
     public double getHargaTiket() {
         return this.harga_tiket;
     }
 
+    /**
+     * method untuk menmasukkan/menampung harga baru
+     * @param hargaBaru
+     */
     public void setHarga(double hargaBaru) {
         this.harga_tiket = hargaBaru;
     }
 
+    /**
+     * method mengembalikan jumlah tiket
+     * @return jummlah tiket
+     */
     public int getJumlahTiket() {
         return this.jumlahTiket;
     }
 
+    /**
+     * Method untuk menampung jumlahtiket
+     * @param jumlahTiket
+     */
     public void setJumlahTiket(int jumlahTiket) {
         this.jumlahTiket = jumlahTiket;
     }
 
+    /**
+     * method abstrak
+     * @param harga
+     * @param jumlah
+     * @return
+     */
     abstract double getTotal(double harga, int jumlah);
 }
